7a96e4307b0d1ac0049b1b3045dc5114772daa61
1 <teykasoc@gmail.com>
2025-06-15 22:18:38 +0200

    commit

--- VacancyProject/VacancyProject/obj/Debug/net7.0/VacancyProject.AssemblyInfo.cs
+++ VacancyProject/VacancyProject/obj/Debug/net7.0/VacancyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VacancyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb098ff24d9b3b95830de8819be072597beb1a4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VacancyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VacancyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
